34e2970c6ca42ddabe3b88ff11f41fe19ae1146b
Mcbronwyn <solinapkarlcyrus@gmail.com>
2026-03-07 22:30:39 +0800

    Initial Commit for The Final Compilation

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,7 @@ import storyEngine.StoryEngine;
 
 public class Game {
     public static void main(String[] args){
-        StoryEngine engine = new StoryEngine();
+       StoryEngine engine = new StoryEngine();
         engine.start();
     }
 }
