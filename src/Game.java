aa5de85044bba73118201d30179ae5988139c75d
Mcbronwyn <solinapkarlcyrus@gmail.com>
2026-05-12 20:38:21 +0800

    Final change pls

--- src/Game.java
+++
@@ -1,8 +0,0 @@
-import storyEngine.StoryEngine;
-
-public class Game {
-    public static void main(String[] args){
-       StoryEngine engine = new StoryEngine();
-        engine.start();
-    }
-}
